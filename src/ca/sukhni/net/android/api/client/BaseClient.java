6eb09b8012d98bf81fab937bb1ec593de501b4e5
Mohammad Alsukhni <malsukhni@mobiroo.com>
2013-08-16 15:57:21 -0400

    - create base classes

---
+++ src/ca/sukhni/net/android/api/client/BaseClient.java
@@ -0,0 +1,6 @@
+package ca.sukhni.net.android.api.client;
+
+public abstract class BaseClient
+{
+
+}
---
+++ src/ca/sukhni/net/android/api/client/RestClient.java
@@ -0,0 +1,6 @@
+package ca.sukhni.net.android.api.client;
+
+public class RestClient extends BaseClient
+{
+
+}
